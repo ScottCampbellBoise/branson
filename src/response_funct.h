97b09b8657653ef646e0e24f9ec058cec28ad2fb
Scott Campbell <scottcampbell.boise@gmail.com>
2019-06-18 11:34:17 -0600

    Fixed bugs in allocating/accessing 2D array

--- src/response_funct.h
+++ src/response_funct.h
@@ -7,55 +7,58 @@
  */
 
 #ifndef response_funct_h
-#deine response_funct_h
+#define response_funct_h
 
 using namespace std;
 
+class Response_Funct {
+
 public:
 
-Response_Funct() {
-    generate_blank_table();
-}
+    Response_Funct() : num_rows(3) , num_cols(3) {
+      generate_blank_table();
+    }   
 
-Response_Funct(int n_rows, int n_cols) : num_rows(n_rows), num_cols(n_cols) {
-    generate_blank_table();
-}
-
-inline double get_response(int row, int col) {
-    if(row >= 0 && row < num_rows && col >= 0 && col < num_cols) {
-        return response_table[row][col];
+    Response_Funct(int n_rows, int n_cols) : num_rows(n_rows), num_cols(n_cols) {
+      generate_blank_table();
     }
-    return -1;
-}
 
-inline bool set_response(int row, int col, double new_response) {
-    if(row >= 0 && row < num_rows && col >= 0 && col < num_cols) {
-        response_table[row][col] = new_response;
-        return true;
+    inline double get_response(int row, int col) {
+      if(row >= 0 && row < num_rows && col >= 0 && col < num_cols) {
+         return response_table[row][col];
+      }
+      return -1;
     }
-    return false;
-}
 
-void set_response_table(int n_rows, int n_cols, double** new_table) {
-    num_rows = n_rows;
-    num_cols = n_cols;
-    response_table = new_table;
-}
+    inline bool set_response(int row, int col, double new_response) {
+      if(row >= 0 && row < num_rows && col >= 0 && col < num_cols) {
+         response_table[row][col] = new_response;
+         return true;
+      }
+      return false;
+    }
 
-void generate_blank_table() {
-    response_table = new double[num_rows];
-    for(int row = 0; row < num_rows; row++) {
-        response_table[row] = new double[num_cols];
-        for(int col = 0; col < num_cols; col++) {
+    void set_response_table(int n_rows, int n_cols, double** new_table) {
+      num_rows = n_rows;
+      num_cols = n_cols;
+      response_table = new_table;
+    }
+
+    void generate_blank_table() {
+      response_table = new double*[num_rows];
+      for(int row = 0; row < num_rows; row++) {
+          response_table[row] = new double[num_cols];
+         for(int col = 0; col < num_cols; col++) {
             response_table[row][col] = 1;
-        }
+         }
+      }
     }
-}
 
 private:
 
-int num_rows = 3;
-int num_cols = 3;
-double** response_table; // Holds the avg. response of each cell
+    int num_rows;
+    int num_cols;
+    double** response_table; // Holds the avg. response of each cell
 
+};
 #endif
