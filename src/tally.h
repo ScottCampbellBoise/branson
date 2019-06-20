4071fe675fe26bf1ecef69889cf4f2035a61a3dd
Scott Campbell <scottcampbell.boise@gmail.com>
2019-06-20 15:27:53 -0600

    Added Input object to get mesh info. Added mesh object variables

--- src/tally.h
+++ src/tally.h
@@ -9,8 +9,9 @@
 #define tally_h
 
 #include <cmath>
-#include "constants.h"
-#include "photon.h"
+#include "constants.h"	// Use for standard math constants
+#include "photon.h"	// Use for particle information: pos, angle, weight
+#include "input.h"	// Contains information about the mesh dimesions
 
 using namespace std;
 using Constants::pi;
@@ -20,16 +21,40 @@ using Constants::pi;
 //     particles only if they travel through the tally
 //     within their last time/space step. Also verified 
 //     the motion filter settings.
+// Test verified to properly get the x and y dimensions
+//     of the mesh object through an Input object
 
 class Tally {
 
 public:
     
-    Tally(double x_1, double y_1, double x_2, double y_2 ) : x_1(x_1), 
-	  y_1(y_1), x_2(x_2), y_2(y_2), n_hits(0), filter_state(NO_FILTER) {
+    // NOTE: The Mesh class jhas been updated to have ref. to Input. Could
+    // 	alternatively pass a mesh as an object, and extract the input
+    // 	out of the mesh object.
+    
+    Tally(double x1, double y1, double x2, double y2, const Input& input) : x1(x1), 
+	  y1(y1), x2(x2), y2(y2), n_hits(0), filter_state(NO_FILTER) {
 	// Define the line equation to connect the two tally points
-        tally_m = (y_1 - y_2) / (x_1 - x_2);
-        tally_b = (-tally_m * x_1) + y_1;
+        tally_m = (y1 - y2) / (x1 - x2);
+        tally_b = (-tally_m * x1) + y1;
+   	
+	// Set up the mesh boundary information
+	mesh_start_x = input.get_x_start(0);
+	mesh_start_y = input.get_y_start(0);
+	mesh_start_z = input.get_z_start(0);
+	mesh_end_x = input.get_x_end(input.get_n_x_divisions() - 1);
+	mesh_end_y = input.get_y_end(input.get_n_y_divisions() - 1);
+	mesh_end_z = input.get_z_end(input.get_n_z_divisions() - 1);     
+
+	// Check that the tally line is fully inside/on the mesh surface
+	if(x1 >= mesh_start_x && x1 <= mesh_end_x &&
+	   x2 >= mesh_start_x && x2 <= mesh_end_x &&
+	   y1 >= mesh_start_y && y1 <= mesh_end_y &&
+	   y2 >= mesh_start_y && y2 <= mesh_end_y) {
+	    std::cout << "\n\nERROR: The tally line MUST be inside or on" <<
+		" the mesh surface!\n\n";
+	    return;
+	}	
     }
     
     ~Tally() {}
@@ -45,21 +70,28 @@ public:
     inline double get_E() { return total_energy; }
     inline void reset_hits() { n_hits = 0; }
     inline void reset_E() { total_energy = 0; }
-    inline double get_x1() { return x_1; }
-    inline double get_y1() { return y_1; }
-    inline double get_x2() { return x_2; }
-    inline double get_y2() { return y_2; }
+    inline double get_x1() { return x1; }
+    inline double get_y1() { return y1; }
+    inline double get_x2() { return x2; }
+    inline double get_y2() { return y2; }
     inline double get_tally_slope() { return tally_m; }
-    inline double get_tally_intcpt() { return tally_b; }
+    inline double get_tally_intercept() { return tally_b; }
     inline int get_motion_filter() { return filter_state; }
 
-    inline set_tally_pos(double x1, double y1, double x2, double y2) {
-	x_1 = x1;
-	y_1 = y1;
-	x_2 = x2;
-	y_2 = x2;
-	tally_m = (y_1 - y_2) / (x_1 - x_1);
-	tally_b = (-tally_m * x_1) + y_1;
+    inline double get_mesh_start_x() { return mesh_start_x; }
+    inline double get_mesh_end_x() { return mesh_end_x; }
+    inline double get_mesh_start_y() { return mesh_start_y; }
+    inline double get_mesh_end_y() { return mesh_end_y; }
+    inline double get_mesh_start_z() { return mesh_start_z; }
+    inline double get_mesh_end_z() { return mesh_end_z; }
+    
+    inline void set_tally_pos(double x1, double y1, double x2, double y2) {
+	x1 = x1;
+	y1 = y1;
+	x2 = x2;
+	y2 = x2;
+	tally_m = (y1 - y2) / (x1 - x1);
+	tally_b = (-tally_m * x1) + y1;
     }
     bool set_motion_filter(int new_filter) {
 	if(new_filter < -1 || new_filter > 1) {
@@ -75,7 +107,11 @@ public:
     const static int NEGATIVE_ONLY_FILTER = -1; // " " in (-) dir
 
 private:
-
+    
+    // ----------------------------------------------------------------------
+    // Private helper methods/structs
+    // ----------------------------------------------------------------------
+    
     struct Point {
 	double x;
 	double y;
@@ -109,8 +145,8 @@ private:
         Point p1 = {phtn_pos[0], phtn_pos[1]};
 	Point q1 = {phtn_prev_pos[0], phtn_prev_pos[1]};
 	
-	Point p2 = {x_1, y_1};
-	Point q2 = {x_2, y_2};
+	Point p2 = {x1, y1};
+	Point q2 = {x2, y2};
 
 	// get the 4 orientations possible
 	double o1 = find_orient(p1, q1, p2); 
@@ -142,11 +178,19 @@ private:
 		q.y <= max(p.y, r.y) && q.y >= min(p.y, r.y));
     }
 
-    double x_1, y_1, x_2, y_2;
+    // ----------------------------------------------------------------------
+    // Private Variables/Constants
+    // ----------------------------------------------------------------------
+    
+    double x1, y1, x2, y2;
     double tally_m, tally_b; // Define the line between the tally y = m*x + b
     int n_hits;
     double total_energy;
     int filter_state;
+    
+    // Variables to store information about the mesh dimensions
+    double mesh_start_x, mesh_start_y, mesh_start_z;
+    double mesh_end_x, mesh_end_y, mesh_end_z;
 
 };
 
