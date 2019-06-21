cb75859e5e7df893723150c2db36ae68f9ee132f
Scott Campbell <scottcampbell.boise@gmail.com>
2019-06-21 16:03:34 -0600

    Added a way to determine if and how far a particle passes through a cell

--- src/response_funct.h
+++ src/response_funct.h
@@ -9,24 +9,54 @@
 #ifndef response_funct_h
 #define response_funct_h
 
+#include <cmath>
+#include <algorithm>
+
+#include "tally.h"
+#include "mesh.h"
+
 using namespace std;
 
 class Response_Funct {
 
-// TO DO:
-//     * Add a reference to a tally surface in const
-//     * Add a way to do ray tracing towards cells, 
-//       based on position on the tally line and angle
-
+// Testing Results:
+// 	The method to check if a line (y = mx + b) passes
+// 	through a cell (box) is test verified to work as 
+// 	expected.
+// To Do:
+// 	for the simple response function table function,
+// 	define lines outward from the tally. NOTE: for
+// 	simplcity, the tally MUST be placed on a cell 
+// 	boundary. V1 will assume its vertical on the 
+// 	rightmost wall. ALSO: each cell must have a
+// 	uniform sigma_a value
+//
+//	** FINISH THE CONTRIBUTION METHOD **
+//
+//	Make a way to find the sigma_a value of a cell ...
 public:
 
-    Response_Funct(Tally& tally, uint32_t n_rays, uint32_t n_tally_points) : 
-		   tally(tally), n_rays(n_rays), n_tally_points(n_tally_points) {
-	mesh_start_x = tally.get_mesh_start_x();
-	mesh_end_x = tally.get_mesh_end_x();
-	mesh_start_y = tally.get_mesh_start_y();
-	mesh_end_y = tally.get_mesh_end_y();
+    struct Point {
+	double x;
+	double y;
+    };
+
+    struct Ray{
+	double m; // Slope of the ray
+	double b; // Y-Intercept of the ray
+    };
 	
+    // Constructor: store the info about the mesh and tally
+    Response_Funct(Tally& tally, Mesh& mesh) : tally(tally),
+					       mesh(mesh) {
+	mesh_start_x = mesh.get_start_x();
+	mesh_end_x = mesh.get_end_x();
+	mesh_start_y = mesh.get_start_y();
+	mesh_end_y = mesh.get_end_y();
+	mesh_dx = mesh.get_dx();
+	mesh_dy = mesh.get_dy();
+	mesh_dz = mesh.get_dz();
+
 	tally_x1 = tally.get_x1();
 	tally_y1 = tally.get_y1();
 	tally_x2 = tally.get_x2();
@@ -35,8 +65,152 @@ public:
 
     ~Response_Funct() {}
 
+    // This uses each cell as a region to accumulate over
+    // Assumes the tally is on the far right wall ...
+    double** get_simple_response(int n_points) {
+	double** resp_table = get_blank_table(mesh.get_n_x_cells(),
+					      mesh.get_n_y_cells());
+	
+	Ray ray;
+	double slopes[3] = {0, -1, 1};
+	
+	for(double point_scale = 0; point_scale <= 1.0; point_scale += (1/n_points)) {
+	    for(int m = 0; m < 3; m++) {
+		// Generate a Ray object that intersects the tally at each point
+		double x_coor = (tally.get_x1() + tally.get_x2()) * point_scale;
+		double y_coor = (tally.get_y1() + tally.get_y2()) * point_scale;
+		
+		// Normalize to the standard grid
+		double adj_y_int = slopes[m] * x_coor - y_coor;
+		ray = {slopes[m] , adj_y_int};
+		
+		// Check all the cells to see if the ray goes through
+		adjust_cells(resp_table, ray);	
+	    }
+
+	}
+	return resp_table;
+    }
+
+    // Iterate through all cells and adjust their table value if the ray goes through
+    void adjust_cells(double**& table, Ray ray) {
+ 	// Do a simple tracking of the photon ...
+    }
+
+    double** get_blank_table(int n_rows, int n_cols) {
+	double** table = new double*[n_rows];
+	for(int row = 0; row < n_rows; row++) {
+	    table[row] = new double[n_cols];
+	    for(int col = 0; col < n_cols; col++) {
+	   	table[row][col] = 0;
+	    }
+	}
+	return table;
+    }
+    
+    double get_contribution(double dist, double sigma_a) {
+	// FINISH THIS SECTION
+	return 0;
+    }
+
+    Point make_point(double x, double y) {
+   	Point p = {x, y};
+	return p;
+    }
+
+    Ray make_ray(double m, double b) {
+	Ray r = {m, b};
+	return r;
+    }
+
+    // Check if a Ray intersects the cell defined by its
+    // lower-left point and its upper right point, and if 
+    // so, return the length of intersection. else -1
+    double intersect_cell(Point l_left, Point u_right, Ray ray) {
+	// Check if the vector intersects the cell
+	Point l_right = {u_right.x, l_left.y};
+	Point u_left = {l_left.x, u_right.y};
+	double l_left_resid = l_left.y - ray.m * l_left.x - ray.b;
+	double l_right_resid = l_right.y - ray.m * l_right.x - ray.b;
+	double u_left_resid = u_left.y - ray.m * u_left.x - ray.b;
+	double u_right_resid = u_right.y - ray.m * u_right.x - ray.b;
+
+	double min_resid = min(l_left_resid, min(l_right_resid, min(u_left_resid, u_right_resid)));
+	double max_resid = max(l_left_resid, max(l_right_resid, max(u_left_resid, u_right_resid)));
+
+	if(min_resid <= 0 && max_resid >= 0) { // ray intersects the cell
+	    // Calculate the distance the ray goes through the cell
+	    
+	    // If the ray is on the edge or goes horiz. through
+	    if(ray.m == 0) { 
+		return (l_right.x - l_left.x);
+	    }
+	    // if the ray is on the edge or eff. vertical through 
+	    else if(ray.m > 1e6) {
+		return (u_left.y - l_left.y);
+	    }
+	    double resid_array[4] = {l_left_resid,l_right_resid,u_right_resid,u_left_resid};
+	    sort(resid_array, resid_array + 4);
+            
+	    //Case 1: three of the residuals are either positive or negative
+	    if(resid_array[0] < 0 && resid_array[1] > 0) {
+		if(resid_array[0] == l_left_resid) {
+		    return sqrt(pow(l_left.x - (l_left.y-ray.b)/ray.m,2) +
+			   pow((ray.m*l_left.x + ray.b) - l_left.y,2));
+		} else if(resid_array[0] == l_right_resid) {
+		    return sqrt(pow(l_right.x - (l_right.y-ray.b)/ray.m,2) +
+			   pow((ray.m*l_right.x + ray.b) - l_right.y,2));
+		} else if(resid_array[0] == u_left_resid) {
+		    return sqrt(pow(u_left.x - (u_left.y-ray.b)/ray.m,2) +
+			   pow((ray.m*u_left.x + ray.b) - u_left.y,2));
+		} else  {
+		    return sqrt(pow(u_right.x - (u_right.y-ray.b)/ray.m,2) +
+			   pow((ray.m*u_right.x + ray.b) - u_right.y,2));
+		}
+	    } else if (resid_array[2] < 0 && resid_array[3] > 0) {
+		if(resid_array[3] == l_left_resid) {
+		    return sqrt(pow(l_left.x - (l_left.y-ray.b)/ray.m,2) +
+			   pow((ray.m*l_left.x + ray.b) - l_left.y,2));
+		} else if(resid_array[3] == l_right_resid) {
+		    return sqrt(pow(l_right.x - (l_right.y-ray.b)/ray.m,2) +
+			   pow((ray.m*l_right.x + ray.b) - l_right.y,2));
+		} else if(resid_array[3] == u_left_resid) {
+		    return sqrt(pow(u_left.x - (u_left.y-ray.b)/ray.m,2) +
+			   pow((ray.m*u_left.x + ray.b) - u_left.y,2));
+		} else  {
+		    return sqrt(pow(u_right.x - (u_right.y-ray.b)/ray.m,2) +
+			   pow((ray.m*u_right.x + ray.b) - u_right.y,2));
+		}
+	    }	
+	    // Case 2: two points have the same parity
+	    else {
+		if(l_left_resid < 0 && l_right_resid < 0 ) {
+		    return sqrt(pow((l_left.x - l_right.x),2) +
+			   pow((ray.m*l_left.x - ray.b)-(ray.m*l_right.x) ,2));
+		} else if(u_left_resid < 0 && u_right_resid < 0) {
+		    return sqrt(pow((u_left.x - u_right.x),2) +
+			   pow((ray.m*u_left.x - ray.b)-(ray.m*u_right.x) ,2));
+		} else if(l_left_resid < 0 && u_right_resid < 0) {	
+		    return sqrt(pow((l_left.x - u_right.x),2) +
+			   pow((ray.m*l_left.x - ray.b)-(ray.m*u_right.x) ,2));
+		} else if(l_left_resid < 0 && u_left_resid < 0) {
+		    return sqrt(pow((l_left.x - u_left.x),2) +
+			   pow((ray.m*l_left.x - ray.b)-(ray.m*u_left.x) ,2));
+		} else if(l_right_resid < 0 && u_right_resid < 0) {
+		    return sqrt(pow((l_right.x - u_right.x),2) +
+			   pow((ray.m*l_right.x - ray.b)-(ray.m*u_right.x) ,2));
+		} else { // lower right and upper left
+		    return sqrt(pow((l_right.x - u_left.x),2) +
+			   pow((ray.m*l_right.x - ray.b)-(ray.m*u_left.x) ,2));
+		}
+	    }
+	}
+	return -1; // Indicate that the ray does not intersect the cell
+    }
+
 private:
 
+    Mesh& mesh;
     Tally& tally;
 
     uint32_t n_rays; 
@@ -44,6 +218,8 @@ private:
 
     double mesh_start_x, mesh_start_y;
     double mesh_end_x, mesh_end_y;
+    double mesh_dx, mesh_dy, mesh_dz;   
+
     double tally_x1, tally_y1;
     double tally_x2, tally_y2;
 
--- src/test/test_mesh.cc
+++ src/test/test_mesh.cc
@@ -12,7 +12,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 
+#include "../photon.h"
+#include "../tally.h"
 #include "../constants.h"
 #include "../input.h"
 #include "../mpi_types.h"
@@ -30,6 +33,12 @@ int main(int argc, char *argv[]) {
     const Info mpi_info;
 
     MPI_Types mpi_types;
+
+
+    string filename("simple_input.xml");
+    Input input(filename, mpi_types);
+    Proto_Mesh mesh(input, mpi_types, mpi_info);
+
 
     using std::cout;
     using std::endl;
@@ -111,6 +120,99 @@ int main(int argc, char *argv[]) {
         nfail++;
       }
     }
+   
+
+    // test the tally surface and counter
+    {
+        bool tally_surface_pass = true;
+
+        double prev_pos_1[3] = {1.2394,1.3421,1};
+        double prev_pos_2[3] = {2.1245,1.6433,1};
+        double prev_pos_3[3] = {1.2234,3.9863,1};
+
+        double pos_1[3] = {3.3253,5.2523,1};       
+        double pos_2[3] = {2.7356,5.3532,1};       
+        double pos_3[3] = {3.8623,3.8325,1};
+       
+        double prev_pos_4[3] = {3.9323,5.0234,1};
+        double prev_pos_5[3] = {2.4852,5.9212,1};
+        double prev_pos_6[3] = {1.0112,0.5634,1};
+
+        double pos_4[3] = {6.0293,6.7652,1}; 
+        double pos_5[3] = {4.1724,5.4621,1};       
+        double pos_6[3] = {1.2189,0.5126,1};       
+
+        Tally* tally = new Tally(1, 5, 3, 1, input); // Create a line tally
+
+	ofstream outfile("RESULTS.txt");
+	outfile << "Mesh Start x: " << tally->get_mesh_start_x() << endl;
+	outfile << "Mesh End x: " << tally->get_mesh_end_x() << endl;\
+        outfile.close();
+   
+        Photon phtn; 
+	// Test the tally get mesh dimension methods
+	if(tally->get_mesh_start_x() != 0) { tally_surface_pass = false; }
+	if(tally->get_mesh_end_x() != 10) { tally_surface_pass = false; }
+
+	if(tally->get_mesh_start_y() != 0) { tally_surface_pass = false; }
+	if(tally->get_mesh_end_y() != 40) { tally_surface_pass = false; }
+
+	// Test the basic tally operation
+       	phtn.set_position(pos_1);
+	phtn.set_prev_position(prev_pos_1);
+       	if(!tally->hit_tally(phtn)) { tally_surface_pass = false; }
+       	
+	phtn.set_position(pos_2);
+	phtn.set_prev_position(prev_pos_2);
+       	if(!tally->hit_tally(phtn)) { tally_surface_pass = false; }
+       	
+	phtn.set_position(pos_3);
+	phtn.set_prev_position(prev_pos_3);
+	if(!tally->hit_tally(phtn)) { tally_surface_pass = false; }
+
+	// Test the motion filter of the tally
+	tally->set_motion_filter(tally->POSITIVE_ONLY_FILTER);
+	
+	phtn.set_position(pos_1);
+	phtn.set_prev_position(prev_pos_1);
+
+	if(!tally->hit_tally(phtn)) { tally_surface_pass = false; }
+	
+	phtn.set_position(prev_pos_1);
+	phtn.set_prev_position(pos_1);
+	if(tally->hit_tally(phtn)) { tally_surface_pass = false; }
+
+	tally->set_motion_filter(tally->NEGATIVE_ONLY_FILTER);
+	
+	phtn.set_position(pos_1);
+	phtn.set_prev_position(prev_pos_1);
+	if(tally->hit_tally(phtn)) { tally_surface_pass = false; }
+
+	phtn.set_position(prev_pos_1);
+	phtn.set_prev_position(pos_1);
+	if(!tally->hit_tally(phtn)) { tally_surface_pass = false; } 
+
+	// Test the cases where the tally shouldn't be triggered
+       	phtn.set_position(pos_4);
+       	phtn.set_prev_position(prev_pos_4);
+	if(tally->hit_tally(phtn)) { tally_surface_pass = false; }
+       
+	phtn.set_position(pos_5); 
+	phtn.set_prev_position(prev_pos_5);
+	if(tally->hit_tally(phtn)) { tally_surface_pass = false; } 
+       	
+	phtn.set_position(pos_6);	
+	phtn.set_prev_position(prev_pos_6);
+	if(tally->hit_tally(phtn)) { tally_surface_pass = false; }
+
+       if (tally_surface_pass) {
+	 cout << "TEST PASSED: tally surface count" << endl;
+       } else {
+	 cout << "TEST FAILED: tally surface count" << endl;
+         nfail++;
+       }
+    }  
+   
 
   } // need to call destructors for mpi_types before MPI_Finalize
 
