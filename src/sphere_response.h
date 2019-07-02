4a2c7ca219f562d322de1e7696e91e4a805fb797
Scott Campbell <scottcampbell.boise@gmail.com>
2019-07-02 16:54:04 -0600

    Updated to include IMC_State and set cells opacity values based on IMC_state

--- src/sphere_response.h
+++ src/sphere_response.h
@@ -30,8 +30,8 @@ class Sphere_Response {
 public:
 
     // Constructor: store the info about the mesh and tally
-    Sphere_Response(Tally tally, Mesh mesh) : tally(tally),
-					     mesh(mesh) {}   
+    Sphere_Response(Tally& tally, Mesh& mesh, IMC_State &imc_state) : tally(tally),
+					     mesh(mesh), imc_state(imc_state) {}   
 
     ~Sphere_Response() {}
 
@@ -59,44 +59,51 @@ public:
 	// Iterate through every cell in the mesh and reset its value	
  	uint32_t n_cell = mesh.get_n_local_cells();
 	for(uint32_t k = 0; k < n_cell; ++k) {
-	    Cell cell = mesh.get_cell(k);
-	    cell.reset_response();
+	    resp_total_sigma_dist[k] = 0.0;
+	    resp_total_dist[k] = 0.0;
 	}
     }
 
     void create_photon(Photon& phtn) {
-	// FOR DEBUGGING USE - REMOVE CONDITIONAL ...	
-	if(!response_set) {
-	    setup_response();
-	    response_set = true;
-	}
-
         // Create a photon on the tally surface	
         double phi = 2 * Constants::pi * rng->generate_random_number();
-	double mu = 1 - 2 * rng->generate_random_number();
-	double theta = acos(mu);
-	double pos[3] = {tally.get_x1() + tally.get_radius()*(cos(phi)*sqrt((1-pow(mu,2)))),
-			 tally.get_y1() + tally.get_radius()*(sin(phi)*sqrt(1-pow(mu,2))),
-			 tally.get_z1() + tally.get_radius()*mu};
+        double mu = 1 - 2 * rng->generate_random_number();
+        double theta = acos(mu);
+        double pos[3] = {tally.get_x1() + tally.get_radius()*(cos(phi)*sqrt((1-pow(mu,2)))),
+       	     	         tally.get_y1() + tally.get_radius()*(sin(phi)*sqrt(1-pow(mu,2))),
+        		 tally.get_z1() + tally.get_radius()*mu};
 
-	// Cosine-distribution for angle
-	double mu_r = sqrt(rng->generate_random_number());
-	double phi_r = 2 * Constants::pi * rng->generate_random_number();
-	double mu_theta = cos(phi_r) * sqrt(1 - pow(mu,2));
-	double mu_phi = sin(phi_r) * sqrt(1 - pow(mu_r,2));
-	
-	double mu_x = sin(theta)*cos(phi)*mu_r + cos(theta)*cos(phi)*mu_theta
-		      - sin(phi)*mu_phi;
-	double mu_y = sin(theta)*sin(phi)*mu_r + cos(theta)*sin(phi)*mu_theta
-		      + cos(phi)*mu_phi;
-	double mu_z = cos(theta)*mu_r - sin(theta)*mu_theta;
-	double angle[3] = {mu_x, mu_y, mu_z};
-	
-	// Set photon values
-	phtn.set_position(pos);
-	phtn.set_angle(angle);
-	phtn.set_cell(get_photons_cell(pos[0],pos[1],pos[2]).get_ID());
+        // Cosine-distribution for angle
+        double mu_r = sqrt(rng->generate_random_number());
+        double phi_r = 2 * Constants::pi * rng->generate_random_number();
+        double mu_theta = cos(phi_r) * sqrt(1 - pow(mu,2));
+        double mu_phi = sin(phi_r) * sqrt(1 - pow(mu_r,2));
+       
+        double mu_x = sin(theta)*cos(phi)*mu_r + cos(theta)*cos(phi)*mu_theta
+        	      - sin(phi)*mu_phi;
+        double mu_y = sin(theta)*sin(phi)*mu_r + cos(theta)*sin(phi)*mu_theta
+        	      + cos(phi)*mu_phi;
+        double mu_z = cos(theta)*mu_r - sin(theta)*mu_theta;
+        double angle[3] = {mu_x, mu_y, mu_z};
+        
+        // Set photon values
+        phtn.set_position(pos);
+        phtn.set_angle(angle);
+        phtn.set_cell(get_photons_cell(pos[0],pos[1],pos[2]));
         phtn.set_group(floor(rng->generate_random_number() * double(BRANSON_N_GROUPS)));
+    }
+
+    void print_response() {
+	ofstream outfile("RESP_TABLE.txt");
+
+ 	uint32_t n_cell = mesh.get_n_local_cells();
+	for(uint32_t k = 0; k < n_cell; ++k) {
+	    outfile << "sig_avg: " << (resp_total_sigma_dist[k] / resp_total_dist[k]) <<
+		       ",\tsigma*dist: " << resp_total_sigma_dist[k] <<
+		       ",\tdist: " << resp_total_dist[k] << endl;
+	}
+	
+	outfile.close();
     }
 
 private:
@@ -107,25 +114,37 @@ private:
 	tally_y = tally.get_y1();
 	tally_z = tally.get_z1();
 
+	// To set the cell opacity values
+        mesh.calculate_photon_energy(imc_state);
+	
 	// Set the random number generator
 	rng = new RNG();
 
-	// Get all the cells that the tally is in
-	int max_x = (int)(tally_r / mesh.get_dx()) + 1; // max cells in x-dir the sphere can be in
-	int max_y = (int)(tally_r / mesh.get_dy()) + 1; // max cells in y-dir the sphere can be in 
-	int max_z = (int)(tally_r / mesh.get_dz()) + 1;	// max cells in z-dir the sphere can be in
-	
-	tally_cells = new Cell[max_x * max_y * max_z]; // list to hold all cells that tally is in
-	int cur_pos = 0;
+        int cur_pos = 0;
 
  	uint32_t n_cell = mesh.get_n_local_cells();
+	Cell temp_tally_cells[n_cell]; 
+
+	resp_total_sigma_dist = new double[n_cell];
+	resp_total_dist = new double[n_cell];
+	
 	for(uint32_t k = 0; k < n_cell; ++k) {
+
+	    resp_total_sigma_dist[k] = 0.0;
+	    resp_total_dist[k] = 0.0;
+
 	    Cell cell = mesh.get_cell(k);
 	    if(tally_intersects_cell(cell)) {
-		tally_cells[cur_pos++] = cell;
+		temp_tally_cells[cur_pos++] = cell;
 	    }
 	}
-	n_tally_cells = cur_pos;
+
+	tally_cells = new Cell[cur_pos];
+	for(int k = 0; k < cur_pos; k++) {
+	    tally_cells[k] = temp_tally_cells[k];
+	}	
+
+	n_tally_cells = cur_pos + 1;
     }
 
     void move_photon(Photon &phtn) {
@@ -140,18 +159,22 @@ private:
         int group;
   	Cell cell;
   	cell_id = phtn.get_cell();
-  	cell = mesh.get_on_rank_cell(cell_id);
+  	cell = mesh.get_cell(cell_id);
 	
   	bool active = true;
   	while (active) {
             group = phtn.get_group();
             sigma_a = cell.get_op_a(group);
-            // get distance to event
+       
+	    // get distance to event
             dist_to_event = cell.get_distance_to_boundary(
                 phtn.get_position(), phtn.get_angle(), surface_cross);
-            // Update the cell dist and sigma_dist
-            cell.add_response(dist_to_event);
-            // update position
+        
+	    // Update the resp dist and sigma_dist
+ 	    resp_total_dist[cell_id] += dist_to_event;
+	    resp_total_sigma_dist[cell_id] += (dist_to_event * sigma_a);
+
+	    // update position
             phtn.move(dist_to_event);
 
             boundary_event = cell.get_bc(surface_cross);
@@ -159,7 +182,7 @@ private:
                 next_cell = cell.get_next_cell(surface_cross);
                 phtn.set_cell(next_cell);
                 cell_id = next_cell;
-                cell = mesh.get_on_rank_cell(cell_id);
+                cell = mesh.get_cell(cell_id);
             } else {
 	        active = false;
             }
@@ -168,21 +191,21 @@ private:
 
     // Check if a point is inside one of the cells
     // that the tally surface is in
-    Cell& get_photons_cell(double x, double y, double z) {
+    uint32_t get_photons_cell(double x, double y, double z) {
 	for(int k = 0; k < n_tally_cells; k++) {
 	    Cell cell = tally_cells[k];
 	    const double* cell_dim = cell.get_node_array();
 	    if(x >= cell_dim[0] && x <= cell_dim[1] &&
 	       y >= cell_dim[1] && y <= cell_dim[2] &&
 	       z >= cell_dim[3] && z <= cell_dim[5]) {
-		return tally_cells[k];
+		return tally_cells[k].get_ID();
 	    } 
 	}
-	return tally_cells[0];
+	return tally_cells[0].get_ID();
     }
 
     // Check if the tally surface intersects the cell
-    bool tally_intersects_cell(Cell cell) {
+    bool tally_intersects_cell(Cell& cell) {
 	const double* cell_dim = cell.get_node_array();
         double dist_squared = pow(tally_r,2);
 	
@@ -207,8 +230,9 @@ private:
 
     bool response_set = false;
 
-    Mesh mesh;
-    Tally tally;
+    Mesh& mesh;
+    Tally& tally;
+    IMC_State &imc_state;
 
     Cell* tally_cells;
     int n_tally_cells;
@@ -216,5 +240,8 @@ private:
     RNG* rng;
 
     double tally_r, tally_x, tally_y, tally_z;
+
+    double* resp_total_sigma_dist;
+    double* resp_total_dist;
 };
 #endif
