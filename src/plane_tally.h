6d285a63c0a3e77771215acae1ed60bc9b09eb91
ScottCampbellBoise <16940659+ScottCampbellBoise@users.noreply.github.com>
2019-08-08 22:58:52 -0600

    Updated the get_dist_to_tally method to work for all cases
    Was returning incorrect values for the false cases and some of the true cases ...

--- src/plane_tally.h
+++ src/plane_tally.h
@@ -49,37 +49,38 @@ public:
         const double* pos = phtn.get_position();
         const double* ang = phtn.get_angle();
 
-	if(ang[0] < 0) {return 1e9;}        
-
-        double vec_12[3] = {v2[0] - v1[0], v2[1] - v1[1], v2[2] - v1[2]};
-        double vec_13[3] = {v3[0] - v1[0], v3[1] - v1[1], v3[2] - v1[2]};
-        
-        double A = vec_12[1]*vec_13[2] - vec_12[2]*vec_13[1];
-        double B = vec_12[2]*vec_13[0] - vec_12[0]*vec_13[2];
-        double C = vec_12[0]*vec_13[1] - vec_12[1]*vec_13[0];
-        double D = A*v1[0] + B*v1[1] + C*v1[2];
-        
-        //if(A*ang[0] + B*ang[1] + C*ang[2] == 0) { return 1e9; } // parallel to plane
-        
-        double t = -(A*pos[0] + B*pos[1] + C*pos[2] + D) / (A*ang[0] + B*ang[1] + C*ang[2]);
-        
-        double x_int = pos[0] - ang[0]*t;
-        double y_int = pos[1] - ang[1]*t;
-        double z_int = pos[2] - ang[2]*t;
-        
-        double dist = sqrt(pow(x_int-pos[0],2) + pow(y_int-pos[1],2) + pow(z_int-pos[2],2));
-
         double x_min = min(v1[0], min(v2[0], min(v3[0], v4[0])));
         double x_max = max(v1[0], max(v2[0], max(v3[0], v4[0])));
         double y_min = min(v1[1], min(v2[1], min(v3[1], v4[1])));
         double y_max = max(v1[1], max(v2[1], max(v3[1], v4[1])));
         double z_min = min(v1[2], min(v2[2], min(v3[2], v4[2])));
-        double z_max = max(v1[2], max(v2[2], max(v3[2], v4[2])));
+        double z_max = max(v1[2], max(v2[2], max(v3[2], v4[2])));   
+
+        double vec_12[3] = {v2[0] - v1[0], v2[1] - v1[1], v2[2] - v1[2]};
+        double vec_13[3] = {v3[0] - v1[0], v3[1] - v1[1], v3[2] - v1[2]};
+        
+	// Equation of the plane: Ax + By + Cz = D
+	    
+        double A = vec_12[1]*vec_13[2] - vec_12[2]*vec_13[1];
+        double B = vec_12[2]*vec_13[0] - vec_12[0]*vec_13[2];
+        double C = vec_12[0]*vec_13[1] - vec_12[1]*vec_13[0];
+        double D = A*v1[0] + B*v1[1] + C*v1[2];
+        
+        if(ang[0] < 0 || pos[0] > x_max || (A*ang[0] + B*ang[1] + C*ang[2] == 0)) {return 1e100;}
+        
+        double d = ((v1[0]-pos[0])*A + (v1[1]-pos[1])*B + (v1[2]-pos[2])*C) /
+                   (ang[0]*A + ang[1]*B + ang[2]*C);
+        
+        double x_int = d*ang[0] + pos[0];
+        double y_int = d*ang[1] + pos[1];
+        double z_int = d*ang[2] + pos[2];
+        
+        double dist = sqrt(pow(x_int-pos[0],2) + pow(y_int-pos[1],2) + pow(z_int-pos[2],2));
 
 	if(y_int <= y_max && y_int >= y_min 
 	   && z_int <= z_max && z_int >= z_min) {return dist;} 
         
-        return 1e9;
+        return 1e100;
     }
     
     void get_point_on_plane(double*& pos) {
